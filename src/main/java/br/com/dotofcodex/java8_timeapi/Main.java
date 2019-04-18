c5be3f0948c4eda1e72036e1912fbe550bd43661
Pedro Ferreira <pedro.carvalho@cialne.com.br>
2019-04-18 16:51:48 -0300

    Adição usos das classes da API Time do Java 8.

--- src/main/java/br/com/dotofcodex/java8_timeapi/Main.java
+++ src/main/java/br/com/dotofcodex/java8_timeapi/Main.java
@@ -1,5 +1,6 @@
 package br.com.dotofcodex.java8_timeapi;
 
+import java.text.SimpleDateFormat;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
@@ -13,6 +14,11 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalQueries;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Classes:
@@ -26,20 +32,67 @@ import java.time.format.DateTimeFormatterBuilder;
  * {@link ZoneOffset}
  * {@link Period}
  * {@link Duration}
+ * {@link Clock}
  * {@link DateTimeFormatter}
  * {@link DateTimeFormatterBuilder}
- * {@link Clock}
+ * 
+ * 
+ * Pre Java 8:
+ * {@link Date}
+ * {@link Calendar}
+ * {@link GregorianCalendar}
+ * {@link SimpleDateFormat}
+ *
  * 
  * Packages: java.time; java.time.chrono; java.time.format; java.time.temporal;
  * java.time.zone;
  * 
- * docs: https://docs.oracle.com/javase/8/docs/api/
- * tutorial: https://www.baeldung.com/java-8-date-time-intro
+ * 
+ * Docs: https://docs.oracle.com/javase/8/docs/api/; https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+ * Tutorial: https://www.baeldung.com/java-8-date-time-intro; https://www.baeldung.com/java-date-to-localdate-and-localdatetime
  */
 public class Main {
 
+	private static final SimpleDateFormat sdf;
+	
+	static {
+		sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+	}
+	
 	public static void main(String[] args) {
-
+		// using instant
+		System.out.println("Instant: ".concat(Instant.now().toString()));
+		
+		// using localdate
+		System.out.println("LocalDate: ".concat(LocalDate.now().toString()));
+		
+		// using localtime
+		System.out.println("LocalTime: ".concat(LocalTime.now().toString()));
+		
+		// using localdatetime
+		System.out.println("LocalDateTime: ".concat(LocalDateTime.now().toString()));
+		
+		// using date (pre java 8)
+		System.out.println("Date: ".concat(sdf.format(new Date())));
+		
+		// using offsetdatetime
+		System.out.println("OffsetDateTime: ".concat(OffsetDateTime.now().toString()));
+		
+		// using zonedatetime
+		System.out.println("ZoneDateTime: ".concat(ZonedDateTime.now().toString()));
+		
+		// using zoneid
+		System.out.println("ZoneId: ".concat(ZoneId.systemDefault().toString()));
+		
+		// using zoneoffset
+		System.out.println("ZoneOffset: ".concat(ZoneId.systemDefault().getRules().getOffset(LocalDateTime.now()).toString()));
+		
+		// using datetimeformatter
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS z");
+		String d = "2019-04-18 23:25:59:159 -03:00";
+		System.out.println("DateTimeFormatter[Date]: ".concat(dtf.parse(d, TemporalQueries.localDate()).toString()));
+		System.out.println("DateTimeFormatter[Time]: ".concat(dtf.parse(d, TemporalQueries.localTime()).toString()));
+		System.out.println("DateTimeFormatter[Zone]: ".concat(dtf.parse(d, TemporalQueries.zone()).toString()));
 	}
 
 }
